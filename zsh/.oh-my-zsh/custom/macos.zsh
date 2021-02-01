d74a56b74e79540ddf7f4f6c9afbc3595eebae5c
Julio Castillo <jccb@google.com>
2021-02-01 10:59:42 +0100

    Execute macos config conditionally

--- zsh/.oh-my-zsh/custom/macos.zsh
+++ zsh/.oh-my-zsh/custom/macos.zsh
@@ -1,3 +1,6 @@
-export CLOUDSDK_PYTHON=python3.9
-source $HOME/google-cloud-sdk/completion.zsh.inc
-. `which env_parallel.zsh`
+if [ "$SHORT_HOST" = "jccb-macbookpro" ] ; then
+    export CLOUDSDK_PYTHON=python3.9
+    export CLOUDSDK_PYTHON_SITEPACKAGES=1
+    source $HOME/google-cloud-sdk/completion.zsh.inc
+    #. `which env_parallel.zsh`
+fi
