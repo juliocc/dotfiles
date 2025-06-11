a5a299f835388ae366667b23d6355d4cb08d3e78
Julio Castillo <jccb@google.com>
2025-06-11 11:56:28 +0200

    Updates

--- zsh/.oh-my-zsh/custom/macos.zsh
+++ zsh/.oh-my-zsh/custom/macos.zsh
@@ -1,5 +1,5 @@
 if [ "$SHORT_HOST" = "jccb-macbookpro3" ] ; then
-    export CLOUDSDK_PYTHON="/Users/jccb/.pyenv/versions/gcloud312/bin/python"
+    export CLOUDSDK_PYTHON="/Users/jccb/bin/gcloudvenv/bin/python"
     export CLOUDSDK_PYTHON_SITEPACKAGES=1
     source $HOME/google-cloud-sdk/completion.zsh.inc
 fi
